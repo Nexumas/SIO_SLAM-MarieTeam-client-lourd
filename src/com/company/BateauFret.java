b7039e86bcb288a5862ea79b1bd712033563fa2b
theo-blampain <71644167+theo-blampain@users.noreply.github.com>
2021-03-22 17:08:51 +0100

    Commit 2 2021-03-22

--- src/com/company/BateauFret.java
+++ src/com/company/BateauFret.java
@@ -3,6 +3,11 @@ package com.company;
 public class BateauFret extends Bateau {
     private int poidMaxBatFret;
 
+    public BateauFret(int id, String nom, float longueur, float largeur, int poid){
+        super(id, nom, longueur, largeur);
+        this.poidMaxBatFret = poid;
+    }
+
     public int getPoidMaxBatFret(){
         return this.poidMaxBatFret;
     }
