57ef73c7445b6721d9ae4f928fbe0118e681dd37
Twolf <tomdsx.59118@gmail.com>
2021-04-04 15:42:41 +0200

    correction

--- src/com/company/Fenetre.java
+++ src/com/company/Fenetre.java
@@ -7,23 +7,29 @@ import java.awt.event.ActionListener;
 
 public class Fenetre extends JFrame implements ActionListener {
 
+    //lance l'affichage de la fenêtre et des composants graphiques
     Fenetre(){
-        JFrame fenetre = new JFrame();
-        fenetre.setSize(800,600);
-        fenetre.setTitle("MarieTeam");
-
         this.setLayout(new FlowLayout());
 
+        radioBoutons();
+
+        this.pack();
+        this.setVisible(true);
+        this.setSize(800,600);
+        this.setTitle("MarieTeam");
+    }
+
+    //initialisation des radioboutons
+    private void radioBoutons(){
         JRadioButton creer = new JRadioButton("creer");
         this.add(creer);
 
 
-        JRadioButton modifier = new JRadioButton();
-
-        this.pack();
-        this.setVisible(true);
+        JRadioButton modifier = new JRadioButton("modifier");
+        this.add(modifier);
     }
 
+    //evenements composants graphiques
     @Override
     public void actionPerformed(ActionEvent e) {
 
