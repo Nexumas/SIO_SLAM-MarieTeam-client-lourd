e98230dd562f059fbac9935a09700454e056346e
theo-blampain <theo.blampain@gastonberger.fr>
2021-05-24 16:23:17 +0200

    Générer frontend

--- src/com/company/Fenetre.java
+++ src/com/company/Fenetre.java
@@ -4,57 +4,61 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import com.company.bdd.Gestionnaire;
 
 public class Fenetre extends JFrame implements ActionListener {
 
-    private Boolean creerIsShown;
-    private Boolean modifierIsShown;
+    private TextField titre = new TextField();
+    private JComboBox selecBat = new JComboBox();
+    private JButton modifier = new JButton();
+    private JButton generer = new JButton();
+    private JButton terminer = new JButton();
+
     //lance l'affichage de la fenêtre et des composants graphiques
     Fenetre(){
-        this.setLayout(new FlowLayout());
+        try{
+            FenInit();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
-        radioBoutons();
+    private void FenInit(){
+        this.getContentPane().setLayout(null);
 
         this.pack();
         this.setVisible(true);
         this.setSize(800,600);
         this.setTitle("MarieTeam");
-        this.creerIsShown = false;
-        this.modifierIsShown = false;
+
+        titre.setBounds(350, 520, 100, 20);
+        titre.setText("MarieTeam");
+
+        selecBat.setBounds(200,480,400, 40);
+        initListBat();
+
+        generer.setBounds(250, 120, 300, 80);
+        generer.setText("GENERER");
+        modifier.setBounds(75, 20, 300, 80);
+        modifier.setText("MODIFIER");
+        terminer.setBounds(425, 20, 300, 80);
+        terminer.setText("TERMINER");
+
     }
 
-    private void texts(){
-        TextField chemin = new TextField();
-        TextField recherche = new TextField();
-        TextField resultat = new TextField();
-        TextField listeBatPDF = new TextField();
-    }
-
-    //initialisation des radioboutons
-    private void radioBoutons(){
-        JRadioButton creer = new JRadioButton("creer");
-        this.add(creer);
-
-        JRadioButton modifier = new JRadioButton("modifier");
-        this.add(modifier);
-
-        ButtonGroup btGroup = new ButtonGroup();
-        btGroup.add(creer);
-        btGroup.add(modifier);
-
-        creer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //this.afficherCreer();
-            }
-        });
-    }
-
-    private void afficherCreer(){
-        if(!(modifierIsShown)){
-
+    private void initListBat(){
+        //Initialise la liste des bateaux disponibles dans la comboBox selecBat
+        Gestionnaire forBatList = new Gestionnaire();
+        forBatList.selectListeBateaux();
+        ArrayList<Bateau> lesBateaux = forBatList.getBatList();
+        for(Bateau unBateau : lesBateaux){
+            selecBat.addItem(unBateau.getNomBat());
         }
     }
+
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
